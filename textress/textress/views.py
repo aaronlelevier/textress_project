1b54373358361d28c30178fff413cb0d32b7131f
aronysidoro <aaron.yy.lelevier@gmail.com>
2015-04-01 07:42:55 -0700

    next: re-connect static resource links n do 404/500 pages

--- textress/textress/views.py
+++ textress/textress/views.py
@@ -5,14 +5,14 @@ from django.template import RequestContext
 ### ERROR PAGES ###
 
 def handler404(request):
-    response = render_to_response('404.html', {},
+    response = render_to_response('error/404.html', {},
         context_instance=RequestContext(request))
     response.status_code = 404
     return response
 
 
 def handler500(request):
-    response = render_to_response('500.html', {},
+    response = render_to_response('error/500.html', {},
         context_instance=RequestContext(request))
     response.status_code = 500
     return response
