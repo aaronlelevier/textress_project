abc372acfdd146aa87022b23b55ba128e984508d
aronysidoro <aaron.yy.lelevier@gmail.com>
2015-04-02 05:32:59 -0700

    contact.Newsletter passing tests

--- textress/contact/tests/test_forms.py
+++ textress/contact/tests/test_forms.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
 
+from model_mommy import mommy
+
+from contact.forms import NewsletterForm
 from contact.models import Newsletter
 from utils.messages import dj_messages
 
@@ -19,5 +23,17 @@ class ComingSoonTests(TestCase):
             follow=True)
         self.assertRedirects(response, reverse('contact:coming_soon'))
         assert Newsletter.objects.first()
-        assert response.context['message'] == dj_messages['coming_soon']
 
+        # dj-messages
+        m = list(response.context['messages'])
+        assert len(m) == 1
+        assert str(m[0]) == dj_messages['coming_soon']
+
+    def test_post_same_email(self):
+        # all emails must be unique
+        nl = mommy.make(Newsletter, email=settings.DEFAULT_EMAIL_SAYHELLO)
+        assert isinstance(nl, Newsletter)
+
+        response = self.client.post(reverse('contact:coming_soon'),
+            {'email': settings.DEFAULT_EMAIL_SAYHELLO})
+        self.assertFormError(response, 'form', 'email', NewsletterForm.error_messages['already'])
