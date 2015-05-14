be5ec514ea47a931217ff25996a74cbddd6c7b0d
aronysidoro <pyaaron@gmail.com>
2015-05-14 06:59:54 -0700

    angularJS works as single page app

---
+++ textress/source/angular/app.js
@@ -0,0 +1,23 @@
+angular.module('phonecatApp', [
+        'ui.router',
+        'ngResource',
+        'phonecatApp.controllers'
+    ])
+    .config(function($interpolateProvider, $httpProvider, $resourceProvider, $locationProvider) {
+        // dif brackets from django
+        $interpolateProvider.startSymbol('[[').endSymbol(']]');
+
+        // Allow AJAX Post
+        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+        // CSRF Support
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+        // This only works in angular 3!
+        // It makes dealing with Django slashes at the end of everything easier.
+        $resourceProvider.defaults.stripTrailingSlashes = false;
+
+        //Removes /#/ URL element
+        $locationProvider.html5Mode(true).hashPrefix('!');
+    });
