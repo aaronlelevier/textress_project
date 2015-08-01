7596a09d31c5218bc39128db7e15e9a375394a25
aronysidoro <pyaaron@gmail.com>
2015-08-01 10:34:42 -0700

    wip one-time-pmt view

--- textress/payment/forms.py
+++ textress/payment/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.utils.translation import ugettext, ugettext_lazy as _
+
+from account.models import CHARGE_AMOUNTS
 
 
 class StripeForm(forms.Form):
     stripe_token = forms.CharField()
+    amount = forms.ChoiceField(choices=CHARGE_AMOUNTS)
