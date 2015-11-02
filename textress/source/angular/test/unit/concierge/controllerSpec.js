edaee1bd0799b8ffa2928df5f66db33859d282ac
aronysidoro <pyaaron@gmail.com>
2015-11-02 06:23:11 -0800

    [AngJs] init CurrentUser / GuestUser Factory tests w/ init value tests.

--- textress/source/angular/test/unit/concierge/controllerSpec.js
+++ textress/source/angular/test/unit/concierge/controllerSpec.js
@@ -30,32 +30,103 @@ describe('GuestListCtrl', function() {
 
 });
 
-describe('ReplyCtrl', function() {
+describe('GuestMessageCtrl', function() {
+   var mockUser = {
+            id: 1,
+            hotel_id: 1
+        },
+        mockGuest = {
+            id: 1,
+        },
+        mockUserSvc, mockGuestSvc, scope, ctrl  ;
 
-    beforeEach(module('conciergeApp'));
+    beforeEach(module('conciergeApp', function($provide) {
+        $provide.value('CurrentUser', mockUser);
+    }));
 
-    beforeEach(module(function($provide) {
-        $provide.service('CurrentUser', function() {
-            return 1;
+    beforeEach(module('conciergeApp', function($provide) {
+        $provide.value('GuestUser', mockGuest);
+    }));
+
+    beforeEach(inject(function(CurrentUser, GuestUser, $rootScope, $controller) {
+        mockUserSvc = CurrentUser;
+        mockGuestSvc = GuestUser;
+        scope = $rootScope.$new();
+        ctrl = $controller('GuestMessageCtrl', {
+            $scope: scope
         });
     }));
 
-    //Getting reference of the mocked service
-    var mockUtilSvc;
+    // CurrentUser Tests
 
-    inject(function(CurrentUser) {
-        mockUtilSvc = CurrentUser;
+    it('CurrentUser.id', function() {
+        expect(mockUserSvc.id).toEqual(mockUser.id);
     });
 
-    beforeEach(inject(function($rootScope, $controller) {
+    it('CurrentUser.hotel_id', function() {
+        expect(mockUserSvc.hotel_id).toEqual(mockUser.hotel_id);
+    });
+
+    // GuestUser Tests
+
+    it('GuestUser.id', function() {
+        expect(mockGuestSvc.id).toEqual(mockGuest.id);
+    })
+
+    it('init values', function() {
+        expect(scope.messages).toEqual({});
+        expect(scope.modal_msg).toEqual(0);
+        expect(scope.user_id).toEqual(1);
+    })
+});
+
+describe('ReplyCtrl', function() {
+   var mockData = {
+            id: 1,
+            hotel_id: 1
+        }, mockUtilSvc, scope, ctrl  ;
+
+    beforeEach(module('conciergeApp', function($provide) {
+        $provide.value('CurrentUser', mockData);
+    }));
+
+    beforeEach(inject(function(CurrentUser, $rootScope, $controller) {
+        mockUtilSvc = CurrentUser;
         scope = $rootScope.$new();
         ctrl = $controller('ReplyCtrl', {
             $scope: scope
         });
     }));
 
-    it('should return value from mock dependency', inject(function(mockUtilSvc) {
-        expect(mockUtilSvc()).toEqual(1);
+    it('init values', function() {
+        expect(scope.hotel_id).toEqual(1);
+        expect(scope.reply).toEqual(null);
+        expect(scope.letter).toEqual(null);
+    })
+});
+
+describe('TriggerCtrl', function() {
+   var mockData = {
+            id: 1,
+            hotel_id: 1
+        }, mockUtilSvc, scope, ctrl  ;
+
+    beforeEach(module('conciergeApp', function($provide) {
+        $provide.value('CurrentUser', mockData);
     }));
 
+    beforeEach(inject(function(CurrentUser, $rootScope, $controller) {
+        mockUtilSvc = CurrentUser;
+        scope = $rootScope.$new();
+        ctrl = $controller('TriggerCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('init values', function() {
+        expect(scope.hotel_id).toEqual(1);
+        expect(scope.reply).toEqual(null);
+        expect(scope.trigger).toEqual(null);
+        expect(scope.trigger_type).toEqual(null);
+    })
 });
