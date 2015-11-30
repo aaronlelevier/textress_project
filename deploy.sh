1a77b0f3b5f5c8d6adb3ea84f259c7aab819c80c
aronysidoro <pyaaron@gmail.com>
2015-11-29 16:15:05 -0800

    [DEPLOY] checklist

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 npm install
 
+echo "DATABASE"
+dropdb textress
+wait
+createdb textress
+
+# this is only ran in dev (not in build script)
+# ./manage.py makemigrations account concierge contact main payment sms utils
+
+./manage.py migrate
+
 wait
 cd textress
 ./manage.py collectstatic --noinput
--- docs/provision_server.rst
+++
@@ -1,9 +0,0 @@
-Provision Server
-----------------
-
-Django Migrations
-
-.. code-block::
-
-    ./manage.py makemigrations account concierge contact main payment sms utils
-    ./manage.py migrate
