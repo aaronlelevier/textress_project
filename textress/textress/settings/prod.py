e86463963a97246a05596418db3648b8fff3dae4
aronysidoro <pyaaron@gmail.com>
2015-12-05 13:58:23 -0800

    [database] use the same server, so set to 'localhost'

--- textress/textress/settings/prod.py
+++ textress/textress/settings/prod.py
@@ -9,19 +9,11 @@ ALLOWED_HOSTS = ['textress.com']
 
 SITE_URL = "textress.com"
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-        'NAME': os.environ['T17_DB_NAME'],
-        'USER': os.environ['T17_DB_USER'],
-        'PASSWORD': os.environ['T17_DB_PASSWORD'], 
-        'HOST': '104.131.57.229', # DB server IP.  [ was prior-> 'localhost', ]
-        'PORT': '5432'
-    }
-}
-
 STRIPE_SECRET_KEY = os.environ['STRIPE_LIVE_SECRET_KEY']
 STRIPE_PUBLIC_KEY = os.environ['STRIPE_LIVE_PUBLIC_KEY']
+
+LOGGING_DIR = '/var/log/django'
+
 
 # HTTPS
 # os.environ['HTTPS'] = "on"
