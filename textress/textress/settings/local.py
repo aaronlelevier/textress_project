b28052f305f26dbd4357c0d755efe16432c916e6
aronysidoro <pyaaron@gmail.com>
2015-12-24 09:44:31 -0800

    [DEBUG TOOLBAR] comment out for now b/c not needing to debug using it right now.

--- textress/textress/settings/local.py
+++ textress/textress/settings/local.py
@@ -11,13 +11,13 @@ INSTALLED_APPS += THIRD_PARTY_APPS
 
 
 ### DJANGO DEBUG TOOLBAR ###
-INSTALLED_APPS += ('debug_toolbar',)
+# INSTALLED_APPS += ('debug_toolbar',)
 
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
+# MIDDLEWARE_CLASSES += (
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# )
 
-INTERNAL_IPS = ('127.0.0.1',)
+# INTERNAL_IPS = ('127.0.0.1',)
 
 
 if 'test' in sys.argv:
